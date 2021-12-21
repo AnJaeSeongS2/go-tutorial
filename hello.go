5dcb20048714768411518829167b1bd276662c12
AnJaeSeongS2 <jindam91@naver.com>
2021-12-21 16:03:14 +0900

    Tour: package 1/17 - add crypto/rand

--- hello.go
+++ hello.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	cryptoRand "crypto/rand"
 	"fmt"
+	"log"
+	"math/big"
+	"math/rand"
 	"time"
 )
 
 func main() {
-	fmt.Println("Hello, jaeseong")
-	fmt.Println("The time is", time.Now())
+	randN := time.Now().UnixNano()
+	rand.Seed(randN)
+	fmt.Println("My favorite first number is ", rand.Intn(10))
+
+	// crypto/rand use CSPRNG. and on linux, it use /dev/urandom (heavy job)
+	cryptoRandN, err := cryptoRand.Int(cryptoRand.Reader, big.NewInt(10))
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("My favorite second number is ", cryptoRandN)
 }
